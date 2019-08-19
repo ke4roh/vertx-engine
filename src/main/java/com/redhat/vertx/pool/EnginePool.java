8ac13c7e7f39e1278ccc24d679c0c209065293b8
Jim Scarborough <jscarbor@redhat.com>
2019-08-19 10:25:05 -0400

    register Engine verticle upon creation

--- src/main/java/com/redhat/vertx/pool/EnginePool.java
+++ src/main/java/com/redhat/vertx/pool/EnginePool.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.redhat.vertx.Engine;
 import io.reactivex.Single;
+import io.vertx.reactivex.core.Vertx;
 
 /**
  * This class is responsible for instantiating enginePool and making them available for execution of their
@@ -14,21 +15,29 @@ public class EnginePool {
     // make this expire things or something - fancier
     PipelineResolver resolver;
     Map<String, Engine> map;
+    Vertx vertx;
 
-    public EnginePool(PipelineResolver resolver) {
+    public EnginePool(PipelineResolver resolver, Vertx vertx) {
         this.resolver = resolver;
         this.map = new HashMap<>();
+        this.vertx = vertx;
     }
 
     public Single<Engine> getEngineByPipelineName(String pipelineName) {
         return Single.create(emitter -> {
             resolver.getExecutablePipelineByName(pipelineName)
                     .subscribe(pipeline -> {
-                                if (!map.containsKey(pipeline)) {
-                                    map.put(pipeline, new Engine(pipeline));
-                                }
+                                Engine e = map.get(pipeline);
 
-                                emitter.onSuccess(map.get(pipeline));
+                                if (e == null) {
+                                    e = new Engine(pipeline);
+                                    final Engine engine=e;
+                                    map.put(pipeline, e);
+                                    vertx.rxDeployVerticle(e).subscribe(s -> emitter.onSuccess(engine));
+                                    // TODO either make Engine a non-verticle, or find some way to un-register it later
+                                } else {
+                                    emitter.onSuccess(e);
+                                }
                             },
                             emitter::tryOnError);
         });
