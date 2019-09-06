568f188a81e261cc3b50631f039cd44928d2ab3f
Jim Scarborough <jscarbor@redhat.com>
2019-09-06 09:09:18 -0400

    more tests, comments

--- src/main/java/com/redhat/vertx/pipeline/templates/TemplateProcessor.java
+++ src/main/java/com/redhat/vertx/pipeline/templates/TemplateProcessor.java
@@ -5,5 +5,11 @@ import io.vertx.core.json.JsonObject;
 import java.util.Map;
 
 public interface TemplateProcessor {
+    /**
+     * @param env The environment from which to look up variables
+     * @param str The string to which to apply the template
+     * @throws MissingParameterException if a variable mentioned in the string cannot be found in the environment.
+     * @return An unmodified string if it contains no template markup, or the modified string with template applied
+     */
     public String applyTemplate(Map<String,Object> env, String str);
 }
