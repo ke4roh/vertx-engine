2d57171f865d776a1b94e9a0c6d650d755a23b34
Jim Scarborough <jscarbor@redhat.com>
2019-09-16 09:07:34 -0400

    cleanup

--- src/main/java/com/redhat/vertx/pipeline/DocBasedDisposableManager.java
+++ src/main/java/com/redhat/vertx/pipeline/DocBasedDisposableManager.java
@@ -5,23 +5,23 @@ import io.reactivex.disposables.Disposable;
 import java.util.*;
 
 public class DocBasedDisposableManager {
-    private Map<String, Collection<Disposable>> disposables = new HashMap<>();
+    private Map<String, Collection<Disposable>> disposablesByDocId = new HashMap<>();
 
 
     public void addDisposable(String documentId, Disposable disposable) {
-        this.addDisposable(documentId,Arrays.asList(disposable));
+        this.addDisposable(documentId,Collections.singletonList(disposable));
     }
 
-    public void addDisposable(String documentId, Collection disposables) {
-        Collection d = this.disposables.getOrDefault(documentId,new ArrayList<>());
-        this.disposables.put(documentId,d);
-        d.addAll(disposables);
+    public void addDisposable(String documentId, Collection<Disposable> disposables) {
+        Collection<Disposable> listOfDisposables = this.disposablesByDocId.getOrDefault(documentId,new ArrayList<>());
+        this.disposablesByDocId.put(documentId,listOfDisposables);
+        listOfDisposables.addAll(disposables);
     }
 
     public void finish(String documentId) {
-        Collection<Disposable> doomed = disposables.remove(documentId);
-        if (doomed != null) {
-            doomed.iterator().forEachRemaining(Disposable::dispose);
+        Collection<Disposable> doomedDisposables = disposablesByDocId.remove(documentId);
+        if (doomedDisposables != null) {
+            doomedDisposables.forEach(Disposable::dispose);
         }
     }
 
