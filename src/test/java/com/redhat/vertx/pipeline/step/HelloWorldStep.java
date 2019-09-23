e959acd842ea5e70a9cbf8cfaeca7be82ce444ed
Jason Porter <lightguard.jp@gmail.com>
2019-09-23 15:40:33 -0600

    HelloWorldStep now extends AbstractStep

--- src/test/java/com/redhat/vertx/pipeline/step/HelloWorldStep.java
+++ src/test/java/com/redhat/vertx/pipeline/step/HelloWorldStep.java
@@ -1,11 +1,9 @@
 package com.redhat.vertx.pipeline.step;
 
-import java.util.Objects;
-
 import com.redhat.vertx.Engine;
+import com.redhat.vertx.pipeline.AbstractStep;
 import com.redhat.vertx.pipeline.Step;
 import io.reactivex.Completable;
-import io.reactivex.Maybe;
 import io.vertx.core.json.JsonObject;
 import org.kohsuke.MetaInfServices;
 
@@ -15,7 +13,7 @@ import org.kohsuke.MetaInfServices;
  * for steps that aren't blocking, abstract away the reactive element.
  */
 @MetaInfServices(Step.class)
-public class HelloWorldStep implements Step {
+public class HelloWorldStep extends AbstractStep {
     private String stepName;
     private String name;
     private String registerTo;
@@ -23,32 +21,14 @@ public class HelloWorldStep implements Step {
 
     @Override
     public Completable init(Engine engine, JsonObject config) {
-        stepName = config.getString("name","HelloWorld");
-        stepConfig = config.getJsonObject(getShortName(), new JsonObject());
-        var vars = stepConfig.getJsonObject("vars", new JsonObject());
-
-        // Just in case there isn't any additional config for a step
-        if (Objects.isNull(stepConfig)) {
-            stepConfig = new JsonObject();
-        }
-
+        super.init(engine, config);
         name = vars.getString("name", "world");
         registerTo = config.getString("register","greeting");
         return Completable.complete();
     }
 
     @Override
-    public Maybe<JsonObject> execute(String uuid) {
-        return Maybe.just(new JsonObject().put(registerTo,"hello, " + name));
-    }
-
-    @Override
-    public String getName() {
-        return stepName;
-    }
-
-    @Override
-    public JsonObject getStepConfig() {
-        return this.stepConfig;
+    public String execute(JsonObject env) {
+        return "hello, " + name;
     }
 }
